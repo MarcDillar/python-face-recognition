b700c188a7ed3e265f077e5d9a2f18408e713d43
mdillar <marc.dillar@gmail.com>
2021-07-04 01:22:33 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='face-recognition',
+    version='0.1.0',
+    description='Simple face recognition for teaching purposes',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Marc Dillar',
+    author_email='marc.dillar@gmail.com',
+    url='https://github.com/MarcDillar/python-face-recognition',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'examples'))
+)
